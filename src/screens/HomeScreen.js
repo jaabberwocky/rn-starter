368ca502324da55dc5e6ffbb350775dc1b8c8a00
tobias <tobias.leong@zendesk.com>
2022-02-04 16:22:38 +0800

    initial changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+    <React.Fragment>
+      <Text style={styles.text}>HomeScreen: Tobias</Text>
+      <Text style={styles.text2}>Hello!!</Text>
+    </React.Fragment>)
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30,
+    fontSize: 44,
   },
+  text2: {
+    fontSize: 32,
+  }
 });
 
 export default HomeScreen;

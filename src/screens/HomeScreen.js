c9d4f45b9b2a4d827e0678f1bad5949bb33d0e73
tobias <tobias.leong@zendesk.com>
2022-02-04 21:38:33 +0800

    Use destructuring in homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = (props) => {
+const HomeScreen = ({navigation}) => {
   return (
     <View>
       <Text style={styles.text}>HomeScreen: Tobias</Text>
       <Text style={styles.text2}>Hello!!</Text>
       <Button
         title="Go to Component Demo"
-        onPress={() => props.navigation.navigate("Component")}>
+        onPress={() => navigation.navigate("Component")}>
       </Button>
       <Button
         title="Go to List Demo"
-        onPress={() => props.navigation.navigate("List")}>
+        onPress={() => navigation.navigate("List")}>
       </Button>
       <Button
         title="Go to Counter Demo"
-        onPress={() => props.navigation.navigate("Counter")}></Button>
+        onPress={() => navigation.navigate("Counter")}></Button>
     </View>)
 };
 
